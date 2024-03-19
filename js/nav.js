8c5fdcf55a7d2895a06e61b4ca0708857238c5c1
Curstantine <curstantine@proton.me>
2024-03-19 09:02:09 +0530

    feat: make nav sticky and change background image in home

--- js/nav.js
+++ js/nav.js
@@ -1,22 +1,23 @@
 export default function initializeNav() {
     const nav = document.getElementsByTagName("nav")[0];
 
-    nav.innerHTML = /*html*/` 
-        <img alt="WaterWafer" src="/assets/logo.svg" width="120" height="40" style="margin-top: 4px" />
+    nav.innerHTML = /*html*/`
+<div class="nav-container">
+    <img alt="WaterWafer" src="/assets/logo.svg" width="120" height="40" style="margin-top: 4px" />
         
-        <div class="spacer"></div>
-        
-        <div class="actions">
-            <a href="/gallery">Gallery</a>
-            <a href="/shop">Shop</a>
-            <a href="/team">Team</a>
-        </div>
-        
-        <div class="action-spacer"></div>
-        
-        <div class="primary-actions">
-            <a href="/donate" class="btn-ghost">Donate</a>
-            <a href="/login" class="btn-primary">Login</a>
-        </div>
-    `;
+    <div class="spacer"></div>
+    
+    <div class="actions">
+        <a href="/gallery">Gallery</a>
+        <a href="/shop">Shop</a>
+        <a href="/team">Team</a>
+    </div>
+    
+    <div class="action-spacer"></div>
+    
+    <div class="primary-actions">
+        <a href="/donate" class="btn-ghost">Donate</a>
+        <a href="/login" class="btn-primary">Login</a>
+    </div>
+</div>`;
 }
