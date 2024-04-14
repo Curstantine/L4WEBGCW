a67276ba86f3cdf8894a6174654e7e1b7d6b361c
hirushi05 <163982753+hirushi05@users.noreply.github.com>
2024-04-14 15:53:15 +0530

    Add files via upload

---
+++ js/Gallery.js
@@ -0,0 +1,11 @@
+function ShowExtendedView(id){
+    var extendedView = document.getElementById('extendedView' + id);
+    if(extendedView.style.display==="none"){
+        extendedView.style.display = "block";
+    } else{
+        extendedView.style.display = "none";
+    }
+}
+
+
+        
